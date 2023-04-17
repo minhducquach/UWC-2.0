969216a10c4b6566bf0b2793ac606a2acd7044f9
minhduc <minhduc16102@gmail.com>
2023-04-17 17:30:50 +0700

    Add MCP-related and Vehicle-related API handlers

--- Back-End/controllers/mod.js
+++ Back-End/controllers/mod.js
@@ -51,34 +51,100 @@ const deleteTask = async (req, res) => {
   } else res.send("Can't delete task");
 };
 const getMCP = async (req, res) => {
-  return;
+  const fileName = modelPath + "/mcps-data.json";
+  const MCPs = objectHandle.readJSON(fileName);
+  const MCPID = req.params.id;
+  const result = MCPs.find(({ id }) => {
+    return id === MCPID;
+  });
+  if (result) {
+    res.json(result);
+    console.log("Get MCP successfully");
+  } else res.send("Can't find MCP");
 };
 const getAllMCPs = async (req, res) => {
-  return;
+  const fileName = modelPath + "/mcps-data.json";
+  const MCPs = objectHandle.readJSON(fileName);
+  if (MCPs) {
+    res.json(MCPs);
+    console.log("Get all MCPs successfully");
+  } else res.send("Can't find any MCP");
 };
 const addMCP = async (req, res) => {
-  return;
+  const fileName = modelPath + "/mcps-data.json";
+  const newObject = req.body;
+  const result = objectHandle.createObject({ fileName, newObject });
+  if (result) {
+    res.json(newObject);
+    console.log("Add MCP successfully");
+  } else res.send("Can't add MCP");
 };
 const updateMCP = async (req, res) => {
-  return;
+  const fileName = modelPath + "/mcps-data.json";
+  const newObject = req.body;
+  const objectID = req.params.id;
+  const result = objectHandle.modifyObject({ fileName, objectID, newObject });
+  if (result) {
+    res.json(newObject);
+    console.log("Update MCP successfully");
+  } else res.send("Can't update MCP");
 };
 const deleteMCP = async (req, res) => {
-  return;
+  const fileName = modelPath + "/mcps-data.json";
+  const objectID = req.params.id;
+  const result = objectHandle.removeObject({ fileName, objectID });
+  if (result) {
+    res.send("Deleted");
+    console.log("Delete MCP successfully");
+  } else res.send("Can't delete MCP");
 };
 const getVehicle = async (req, res) => {
-  return;
+  const fileName = modelPath + "/vehicles-data.json";
+  const Vehicles = objectHandle.readJSON(fileName);
+  const VehicleID = req.params.id;
+  const result = Vehicles.find(({ id }) => {
+    return id === VehicleID;
+  });
+  if (result) {
+    res.json(result);
+    console.log("Get vehicle successfully");
+  } else res.send("Can't find vehicle");
 };
 const getAllVehicles = async (req, res) => {
-  return;
+  const fileName = modelPath + "/vehicles-data.json";
+  const Vehicles = objectHandle.readJSON(fileName);
+  if (Vehicles) {
+    res.json(Vehicles);
+    console.log("Get all vehicles successfully");
+  } else res.send("Can't find any vehicle");
 };
 const addVehicle = async (req, res) => {
-  return;
+  const fileName = modelPath + "/vehicles-data.json";
+  const newObject = req.body;
+  const result = objectHandle.createObject({ fileName, newObject });
+  if (result) {
+    res.json(newObject);
+    console.log("Add vehicle successfully");
+  } else res.send("Can't add vehicle");
 };
 const updateVehicle = async (req, res) => {
-  return;
+  const fileName = modelPath + "/vehicles-data.json";
+  const newObject = req.body;
+  const objectID = req.params.id;
+  const result = objectHandle.modifyObject({ fileName, objectID, newObject });
+  if (result) {
+    res.json(newObject);
+    console.log("Update vehicle successfully");
+  } else res.send("Can't update vehicle");
 };
 const deleteVehicle = async (req, res) => {
-  return;
+  const fileName = modelPath + "/vehicles-data.json";
+  const objectID = req.params.id;
+  const result = objectHandle.removeObject({ fileName, objectID });
+  if (result) {
+    res.send("Deleted");
+    console.log("Delete vehicle successfully");
+  } else res.send("Can't delete vehicle");
 };
 
 export default {

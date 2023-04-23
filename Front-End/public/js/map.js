069ea1a883f68ce405f1af9c4536568a62d55e54
minhduc <minhduc16102@gmail.com>
2023-04-23 21:09:26 +0700

    Fix map UI

--- Front-End/public/js/map.js
+++ Front-End/public/js/map.js
@@ -1,9 +1,26 @@
 //select block
 import mapUtil from "./map_util.js";
 
+// console.log(window.location.search);
+// const taskCode = window.location.search.substring(4, 12);
+// console.log(taskCode);
+
 console.log(window.location.search);
-const taskCode = window.location.search.substring(4, 12);
-console.log(taskCode);
+const taskCode = new URLSearchParams(window.location.search).get("id");
+const insertIDTask = `<div class="title_list">Xem chi tiết công việc</div>
+<a href="/tasks/generalInfo?id=${taskCode}" class="item_nav">Thông tin tổng quan</a>
+<a href="/tasks/Staff?id=${taskCode}" class="item_nav">Danh sách nhân
+viên</a>
+<a href="./MCP?id=${taskCode}" class="item_nav">Danh sách điểm thu gom</a>
+<a href="./route?id=${taskCode}" class="item_nav" style="background-color: #007777; color: #ffff">Tuyến đường di
+chuyển</a>`;
+document.querySelector(".view_detail").innerHTML = insertIDTask;
+const insertIDUpdate = `
+<div class="title_list">Chỉnh sửa công việc</div>
+<a class="item_nav update-task-link" href="/tasks/updateTask?id=${taskCode}">Chỉnh sửa công việc</a>
+<button id="delete" class = "item_nav"style = "color: #d82f2f;">Xóa công việc</button>
+`;
+document.querySelector(".update-task").innerHTML = insertIDUpdate;
 
 let task_data;
 let task = await fetch(`/tasks/getTask/${taskCode}`)
@@ -54,4 +71,20 @@ $(document).ready(function () {
       '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(mapObj);
   mapUtil.getRoute(mapObj, dataRoute);
+});
+
+const deleteBtn = document.getElementById("delete");
+deleteBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
+  if (confirm("Bạn có chắc muốn xóa công việc này ?") == true) {
+    await fetch(`/tasks/deleteTask/${taskCode}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    alert("Xóa công việc thành công");
+    window.location.href = "/tasks";
+  }
 });
